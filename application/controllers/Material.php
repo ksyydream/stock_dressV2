722da25e9d2c9aae2c77b260d1be10a189cf65d3
ksyydream <249499109@qq.com>
2018-01-18 21:35:21 +0800

    11

--- application/controllers/Material.php
+++ application/controllers/Material.php
@@ -68,11 +68,10 @@ class Material extends MY_Controller {
 	public function save_material_color(){
 		echo $this->material_model->save_material_color();
 	}
-//面料库存
+//面料入库单
 
 	public function list_stock_in($page=1){
 		$data = $this->material_model->list_stock_in($page);
-		$types = $this->material_model->list_material();
 		$cust = $this->cust_model->get_cust_all(1);
 		$this->assign('cust', $cust);
 		$base_url = "/material/list_stock_in/";
@@ -80,11 +79,12 @@ class Material extends MY_Controller {
 		$this->assign('pager', $pager);
 		$this->assign('data', $data);
 		$this->assign('page', $page);
-		$this->assign('types', $types);
 		$this->show('material/list_stock_in');
 	}
 
 	public function add_stock($id=null,$page=1){
+		$today = date('Y-m-d',time());
+		$this->assign('today', $today);
 		$this->assign('page', $page);
 		$cust = $this->cust_model->get_cust_all(1);
 		$material = $this->material_model->get_material_all(1);
@@ -92,10 +92,6 @@ class Material extends MY_Controller {
 		$this->assign('material', $material);
 		$this->assign('color', $color);
 		$this->assign('cust', $cust);
-		if($id){
-			$detail = $this->material_model->get_new_detail($id);
-			$this->assign('detail', $detail);
-		}
 		$this->show('material/stock_in');
 	}
 
@@ -111,5 +107,80 @@ class Material extends MY_Controller {
 		}else{
 			$this->show_message('保存失败!');
 		}
+	}
+
+	public function view_in($id,$page=1){
+		if(!$id)
+			$this->show_message('未找到信息!');
+		$this->assign('page', $page);
+		$detail = $this->material_model->get_in_detail($id);
+		if(!$detail){
+			$this->show_message('未找到信息!');
+		}
+		$this->assign('detail', $detail);
+		$this->show('material/stock_in_view');
+	}
+
+	//面料库存
+	public function list_stock($page=1){
+		$data = $this->material_model->list_stock($page);
+		$material = $this->material_model->get_material_all(1);
+		$cust = $this->cust_model->get_cust_all(1);
+		$color = $this->material_model->get_color_all(1);
+		$this->assign('material', $material);
+		$this->assign('cust', $cust);
+		$this->assign('color', $color);
+		$base_url = "/material/list_stock/";
+		$pager = $this->pagination->getPageLink($base_url, $data['total'], $data['limit']);
+		$this->assign('pager', $pager);
+		$this->assign('data', $data);
+		$this->assign('page', $page);
+		$this->show('material/list_stock');
+	}
+
+	public function down_excel_stock(){
+		$data_res = $this->material_model->get_stock4excel();
+
+		require_once (APPPATH . 'libraries/PHPExcel/PHPExcel.php');
+		$excel  = new \PHPExcel ();
+
+
+		$letter = array('A','B','C','D','E','F','G','H','I','J','K');
+		$tableheader = array('面料型号','面料所属','面料颜色','缸号','原始米数','剩余米数','入库时间','','','','');
+		for($i = 0;$i < count($tableheader);$i++) {
+			$excel->getActiveSheet()->setCellValue("$letter[$i]1","$tableheader[$i]");
+			if($i == 4 || $i == 5)
+				$excel->getActiveSheet()->getColumnDimension("$letter[$i]")->setWidth(15);
+			else
+				$excel->getActiveSheet()->getColumnDimension("$letter[$i]")->setWidth(25);
+			$excel->getActiveSheet()->getStyle("$letter[$i]1")->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+		}
+		$excel->getActiveSheet()->getStyle( 'A1:G1')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
+		$excel->getActiveSheet()->getStyle('A1:G1')->getFill()->getStartColor()->setARGB('FF66CD00');
+		$data = array();
+
+		foreach ($data_res as $k=>$v){
+			$data[] = array($v['material_name'],$v['cust_name'],$v['color_name'],$v['ganghao'],$v['original_meters'],$v['turn_meters'],$v['in_date'],'','','','');
+		}
+
+		for ($i = 2;$i <= count($data) + 1;$i++) {
+			$j = 0;
+			foreach ($data[$i - 2] as $key=>$value) {
+				$excel->getActiveSheet()->setCellValue("$letter[$j]$i","$value",PHPExcel_Cell_DataType::TYPE_STRING);
+				$j++;
+			}
+		}
+		$excel->getActiveSheet()->setTitle('面料库存报表');
+		$write = new \PHPExcel_Writer_Excel5 ($excel);
+		header("Pragma: public");
+		header("Expires: 0");
+		header("Cache-Control:must-revalidate, post-check=0, pre-check=0");
+		header("Content-Type:application/force-download");
+		header("Content-Type:application/vnd.ms-execl");
+		header("Content-Type:application/octet-stream");
+		header("Content-Type:application/download");;
+		header('Content-Disposition:attachment;filename="'.'企业导出'.date('Y-m-d H:i:s',time()).'.xls"');
+		header("Content-Transfer-Encoding:binary");
+		$write->save('php://output');
 	}
 }
--- application/models/Material_model.php
+++ application/models/Material_model.php
@@ -227,16 +227,24 @@ class Material_model extends MY_Model
 		$this->db->join('material_in_form_detail c','c.form_id = a.id','left');
 		$this->db->join('material d','c.material_id = d.id','left');
 		$this->db->join('material_color e','c.color_id = e.id','left');
-		if($this->input->post('keyword')){
-			$this->db->like('a.form_name',$this->input->post('keyword'));
+		if($this->input->post('material_id') && $this->input->post('material_id')!='-2'){
+			$this->db->where('a.material_id',$this->input->post('material_id'));
 		}
-		if($this->input->post('cust_id')){
+		if($this->input->post('cust_id') && $this->input->post('cust_id')!='-2'){
 			$this->db->where('a.cust_id',$this->input->post('cust_id'));
+		}
+		if($this->input->post('color_id') && $this->input->post('color_id')!='-2'){
+			$this->db->where('a.color_id',$this->input->post('color_id'));
 		}
 		if($this->input->post('flag')){
 			$this->db->where("a.flag",$this->input->post('flag'));
 		}
-
+		if($this->input->POST('Istart_date')) {
+			$this->db->where('DATE_FORMAT(a.in_date,\'%Y-%m-%d\') >=', $this->input->POST('Istart_date'));
+		}
+		if($this->input->POST('Iend_date')) {
+			$this->db->where('DATE_FORMAT(a.in_date,\'%Y-%m-%d\') <=', $this->input->POST('Iend_date'));
+		}
 		$num = $this->db->get()->row();
 		$data['total'] = $num->num;
 
@@ -244,20 +252,31 @@ class Material_model extends MY_Model
 		$data['flag'] = $this->input->post('flag')?$this->input->post('flag'):null;
 		$data['cust_id'] = $this->input->post('cust_id')?$this->input->post('cust_id'):null;
 		$data['keyword'] = $this->input->post('keyword')?$this->input->post('keyword'):null;
+		$data['Istart_date'] = $this->input->post('Istart_date')?$this->input->post('Istart_date'):null;
+		$data['Iend_date'] = $this->input->post('Iend_date')?$this->input->post('Iend_date'):null;
 		//获取详细列
 		$this->db->select("a.*,IFNULL(b.name,'公共库存') cust_name,group_concat(distinct d.material_name ORDER BY d.material_name) m_list,count(c.id) as count_all")->from('material_in_form a');
 		$this->db->join('cust b','a.cust_id = b.id','left');
 		$this->db->join('material_in_form_detail c','c.form_id = a.id','left');
 		$this->db->join('material d','c.material_id = d.id','left');
 		$this->db->join('material_color e','c.color_id = e.id','left');
-		if($this->input->post('keyword')){
-			$this->db->like('a.form_name',$this->input->post('keyword'));
+		if($this->input->post('material_id') && $this->input->post('material_id')!='-2'){
+			$this->db->where('a.material_id',$this->input->post('material_id'));
 		}
-		if($this->input->post('cust_id')){
+		if($this->input->post('cust_id') && $this->input->post('cust_id')!='-2'){
 			$this->db->where('a.cust_id',$this->input->post('cust_id'));
+		}
+		if($this->input->post('color_id') && $this->input->post('color_id')!='-2'){
+			$this->db->where('a.color_id',$this->input->post('color_id'));
 		}
 		if($this->input->post('flag')){
 			$this->db->where("a.flag",$this->input->post('flag'));
+		}
+		if($this->input->POST('Istart_date')) {
+			$this->db->where('DATE_FORMAT(a.in_date,\'%Y-%m-%d\') >=', $this->input->POST('Istart_date'));
+		}
+		if($this->input->POST('Iend_date')) {
+			$this->db->where('DATE_FORMAT(a.in_date,\'%Y-%m-%d\') <=', $this->input->POST('Iend_date'));
 		}
 		$this->db->group_by('a.id');
 		$this->db->limit($data['limit'], $offset = ($page - 1) * $data['limit']);
@@ -275,6 +294,7 @@ class Material_model extends MY_Model
 		$meters_list= $this->input->post('meters');
 		$cust_id = $this->input->post('cust_id');
 		$remark = $this->input->post('remark');
+		$in_date = $this->input->post('in_date');
 		$stock_list = array();
 		if(!is_array($material_ids)){
 			return -2;
@@ -283,6 +303,7 @@ class Material_model extends MY_Model
 			foreach($material_ids as $idx => $material_id) {
 				$stock_ = array(
 					'material_id' => $material_id,
+					'in_date'=>$in_date,
 					'color_id' => $color_ids[$idx],
 					'ganghao' => trim($ganghaos[$idx]),
 					'original_meters' => trim($meters_list[$idx]),
@@ -310,6 +331,7 @@ class Material_model extends MY_Model
 		$data_form = array(
 			'cust_id'=>	$cust_id,
 			'remark'=>	$remark,
+			'in_date'=>$in_date,
 			'create_date'=>date('Y-m-d H:i:s'),
 			'modify_date'=>date('Y-m-d H:i:s'),
 			'create_user'=>$this->session->userdata('user_id'),
@@ -338,5 +360,181 @@ class Material_model extends MY_Model
 		}
 
 	}
+
+	public function get_in_detail($id){
+
+		$this->db->select('a.*,IFNULL(b.name,\'公共库存\') cust_name')->from('material_in_form a');
+		$this->db->join('cust b','a.cust_id = b.id','left');
+		$this->db->where(array(
+			'a.id'=>$id
+		));
+		$data = $this->db->get()->row_array();
+		if(!$data){
+			return -1;
+		}
+		$this->db->select('a.*,d.material_name,e.color_name')->from('material_in_form_detail a');
+		$this->db->join('material d','a.material_id = d.id','left');
+		$this->db->join('material_color e','a.color_id = e.id','left');
+		$this->db->where(array(
+			'a.form_id'=>$id
+		));
+		$data['list']=$this->db->get()->result_array();
+		return $data;
+	}
+
+	//面料库存
+	/*
+	public function list_stock($page){
+		$data['limit'] = $this->limit;
+		//获取总记录数
+		$this->db->select('count(distinct(a.id)) as num')->from('material a');
+		$this->db->join('cust b','a.cust_id = b.id','left');
+		$this->db->join('material_stock c','a.id = c.material_id','left');
+		$this->db->join('material_color e','a.color_id = e.id','left');
+		if($this->input->post('keyword')){
+			$this->db->like('a.material_name',$this->input->post('keyword'));
+		}
+		if($this->input->post('cust_id')){
+			$this->db->where('a.cust_id',$this->input->post('cust_id'));
+		}
+		if($this->input->post('flag')){
+			$this->db->where("a.flag",$this->input->post('flag'));
+		}
+
+		$num = $this->db->get()->row();
+		$data['total'] = $num->num;
+
+		//搜索条件
+		$data['flag'] = $this->input->post('flag')?$this->input->post('flag'):null;
+		$data['cust_id'] = $this->input->post('cust_id')?$this->input->post('cust_id'):null;
+		$data['keyword'] = $this->input->post('keyword')?$this->input->post('keyword'):null;
+		//获取详细列
+		$this->db->select("a.material_id,c.material_name,IFNULL(b.name,'公共库存') cust_name,group_concat(distinct e.color_name ORDER BY e.color_name) c_list,count(c.id) as count_all")->from('material a');
+		$this->db->join('cust b','a.cust_id = b.id','left');
+		$this->db->join('material_stock c','c.material_id = a.id','left');
+		$this->db->join('material_color e','a.color_id = e.id','left');
+		if($this->input->post('keyword')){
+			$this->db->like('a.material_name',$this->input->post('keyword'));
+		}
+		if($this->input->post('cust_id')){
+			$this->db->where('a.cust_id',$this->input->post('cust_id'));
+		}
+		if($this->input->post('flag')){
+			$this->db->where("a.flag",$this->input->post('flag'));
+		}
+		$this->db->group_by('a.material_id,c.material_name,b.name');
+		$this->db->limit($data['limit'], $offset = ($page - 1) * $data['limit']);
+		$this->db->order_by('a.id','desc');
+		$data['items'] = $this->db->get()->result_array();
+
+
+		return $data;
+	}
+	*/
+
+	public function list_stock($page){
+		$data['limit'] = $this->limit;
+		//获取总记录数
+		$this->db->select('count(1) num')->from('material_stock a');
+		$this->db->join('cust b','a.cust_id = b.id','left');
+		$this->db->join('material d','a.material_id = d.id','left');
+		$this->db->join('material_color e','a.color_id = e.id','left');
+		if($this->input->post('material_id') && $this->input->post('material_id')!='-2'){
+			$this->db->where('a.material_id',$this->input->post('material_id'));
+		}
+		if($this->input->post('cust_id') && $this->input->post('cust_id')!='-2'){
+			$this->db->where('a.cust_id',$this->input->post('cust_id'));
+		}
+		if($this->input->post('color_id') && $this->input->post('color_id')!='-2'){
+			$this->db->where('a.color_id',$this->input->post('color_id'));
+		}
+		if($this->input->post('flag')){
+			$this->db->where("a.flag",$this->input->post('flag'));
+		}
+		if($this->input->post('ganghao')){
+			$this->db->like("a.ganghao",$this->input->post('ganghao'));
+		}
+		if($this->input->POST('Istart_date')) {
+			$this->db->where('DATE_FORMAT(a.in_date,\'%Y-%m-%d\') >=', $this->input->POST('Istart_date'));
+		}
+		if($this->input->POST('Iend_date')) {
+			$this->db->where('DATE_FORMAT(a.in_date,\'%Y-%m-%d\') <=', $this->input->POST('Iend_date'));
+		}
+		$num = $this->db->get()->row();
+		$data['total'] = $num->num;
+
+		//搜索条件
+		$data['flag'] = $this->input->post('flag')?$this->input->post('flag'):null;
+		$data['ganghao'] = $this->input->post('ganghao')?$this->input->post('ganghao'):null;
+		$data['cust_id'] = $this->input->post('cust_id')?$this->input->post('cust_id'):null;
+		$data['color_id'] = $this->input->post('color_id')?$this->input->post('color_id'):null;
+		$data['material_id'] = $this->input->post('material_id')?$this->input->post('material_id'):null;
+		$data['Istart_date'] = $this->input->post('Istart_date')?$this->input->post('Istart_date'):null;
+		$data['Iend_date'] = $this->input->post('Iend_date')?$this->input->post('Iend_date'):null;
+		//获取详细列
+		$this->db->select("a.*,d.material_name,e.color_name,IFNULL(b.name,'公共库存') cust_name")->from('material_stock a');
+		$this->db->join('cust b','a.cust_id = b.id','left');
+		$this->db->join('material d','a.material_id = d.id','left');
+		$this->db->join('material_color e','a.color_id = e.id','left');
+		if($this->input->post('material_id') && $this->input->post('material_id')!='-2'){
+			$this->db->where('a.material_id',$this->input->post('material_id'));
+		}
+		if($this->input->post('cust_id') && $this->input->post('cust_id')!='-2'){
+			$this->db->where('a.cust_id',$this->input->post('cust_id'));
+		}
+		if($this->input->post('color_id') && $this->input->post('color_id')!='-2'){
+			$this->db->where('a.color_id',$this->input->post('color_id'));
+		}
+		if($this->input->post('flag')){
+			$this->db->where("a.flag",$this->input->post('flag'));
+		}
+		if($this->input->post('ganghao')){
+			$this->db->like("a.ganghao",$this->input->post('ganghao'));
+		}
+		if($this->input->POST('Istart_date')) {
+			$this->db->where('DATE_FORMAT(a.in_date,\'%Y-%m-%d\') >=', $this->input->POST('Istart_date'));
+		}
+		if($this->input->POST('Iend_date')) {
+			$this->db->where('DATE_FORMAT(a.in_date,\'%Y-%m-%d\') <=', $this->input->POST('Iend_date'));
+		}
+		$this->db->limit($data['limit'], $offset = ($page - 1) * $data['limit']);
+		$this->db->order_by('a.in_date','desc');
+		$data['items'] = $this->db->get()->result_array();
+
+
+		return $data;
+	}
+
+	public function get_stock4excel(){
+		$this->db->select("a.*,d.material_name,e.color_name,IFNULL(b.name,'公共库存') cust_name")->from('material_stock a');
+		$this->db->join('cust b','a.cust_id = b.id','left');
+		$this->db->join('material d','a.material_id = d.id','left');
+		$this->db->join('material_color e','a.color_id = e.id','left');
+		if($this->input->post('material_id') && $this->input->post('material_id')!='-2'){
+			$this->db->where('a.material_id',$this->input->post('material_id'));
+		}
+		if($this->input->post('cust_id') && $this->input->post('cust_id')!='-2'){
+			$this->db->where('a.cust_id',$this->input->post('cust_id'));
+		}
+		if($this->input->post('color_id') && $this->input->post('color_id')!='-2'){
+			$this->db->where('a.color_id',$this->input->post('color_id'));
+		}
+		if($this->input->post('flag')){
+			$this->db->where("a.flag",$this->input->post('flag'));
+		}
+		if($this->input->post('ganghao')){
+			$this->db->like("a.ganghao",$this->input->post('ganghao'));
+		}
+		if($this->input->POST('Istart_date')) {
+			$this->db->where('DATE_FORMAT(a.in_date,\'%Y-%m-%d\') >=', $this->input->POST('Istart_date'));
+		}
+		if($this->input->POST('Iend_date')) {
+			$this->db->where('DATE_FORMAT(a.in_date,\'%Y-%m-%d\') <=', $this->input->POST('Iend_date'));
+		}
+		$this->db->order_by('a.in_date','desc');
+		$data = $this->db->get()->result_array();
+		return $data;
+	}
+
 
 }
